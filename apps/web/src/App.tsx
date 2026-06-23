b401e291d25eda914e88436949d7037170ed92fc
Ethan Li <Ethan7586@gsyen.com>
2026-06-24 05:21:36 +0800

    Remove Tempora route from PrismEdge

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,5 @@
-import { lazy, Suspense } from "react";
 import PrismEdgeTerminal from "./PrismEdgeTerminal";
 
-const TemporaFlip = lazy(() => import("./tempora/TemporaFlip"));
-
 export default function App() {
-  const isTemporaRoute = window.location.pathname.startsWith("/tempora");
-  return isTemporaRoute ? (
-    <Suspense fallback={null}>
-      <TemporaFlip />
-    </Suspense>
-  ) : <PrismEdgeTerminal />;
+  return <PrismEdgeTerminal />;
 }
